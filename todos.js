b01adc1bd5f8f6898b6d010aab141dd063fe9034
AlvarinoLemes <alvarinoguilherme@hotmail.com>
2020-05-03 14:50:39 -0300

    att

--- todos.js
+++ todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar',
-    'Jogar e F1'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -40,6 +36,7 @@ function addtodo() {
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addtodo;
@@ -47,4 +44,9 @@ buttonElement.onclick = addtodo;
 function deletetodo(pos) {
     todos.splice(pos, 1);
     renderTodos();  
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos))
 }
